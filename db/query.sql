8a262f6c99ddb439c2e29bc6952bc8817a94d8fa
shelldan <shelldanlee@gmail.com>
2022-10-24 13:51:41 -0500

    join more than 2 tables

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,15 @@
-SELECT employee_id, first_name, last_name, role_id, manager_id
-FROM employees AS Employee
-JOIN employees AS Manager
-ON Employee.manager_id = Manager.employee_id
+SELECT employees.employee_id, 
+       employees.first_name, 
+       employees.last_name,
+       roles.title, 
+       departments.department,
+       roles.salary,
+       employees.role_id, 
+       employees.manager_id, 
+       CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employees 
+LEFT JOIN employees manager
+ON manager.employee_id = employees.manager_id,
+FROM employees
+JOIN roles
+ON employees.role_id = roles.role_id;
