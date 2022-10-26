83ba535eaab3cb61b10954e24ca21ed9fb0e7925
shelldan <shelldanlee@gmail.com>
2022-10-25 23:12:33 -0500

    update addEmploye()

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const inquirer = require('inquirer')
 const {connection} = require('./config/connection.js')
 
 let departmentsArray = [] //to store department
-let roles = [] //to store role
-let employees = [] //to store employees 
-let manager = [] //to store manager
+let rolesArray = [] //to store role
+let employeesArray = [] //to store employees 
+let managersArray = [] //to store manager
 
 askUserInput = () =>{
     return inquirer
@@ -164,14 +164,17 @@ addRole = () =>{
                 }
             ])
             .then((answer)=>{
-                let sql = `INSERT INTO roles (title, department, salary) VALUES (?, ?, ?)`
+                let sql = `INSERT INTO roles (title, department_id, salary) VALUES (?, ?, ?)`
                 let params = [answer.newRole, answer.selectDepartment, answer.newSalary]
+                console.log(params)
+                console.log(answer.selectDepartment)
                 connection.query(sql, params, (err, result) =>{
                     if(err){
                         console.log(err);
                     }
                     viewAllRoles();
                 })
+                
             })
 
     })
@@ -179,57 +182,97 @@ addRole = () =>{
 
 
 addEmployee = () =>{
-    return inquirer
-        .prompt ([
+    connection.query(`SELECT 
+                      employees.employee_id, 
+                      employees.first_name, 
+                      employees.last_name,
+                      employees.role_id,
+                      roles.title,
+                      employees.manager_id,
+                      departments.department,
+                      roles.salary,
+                      CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+                      FROM employees 
+                      LEFT JOIN employees manager
+                      ON manager.employee_id = employees.manager_id
+                      LEFT JOIN roles
+                      ON employees.role_id = roles.role_id
+                      LEFT JOIN departments 
+                      ON roles.department_id = departments.department_id;`,(err, res)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(res)
+
+        employeesArray = res.map(emp =>(
             {
-                type: 'input',
-                name: 'newFirstName',
-                message: "What is the employee's first name?",
-            },
-            {
-                type: 'input',
-                name: 'newLastName',
-                message: "What is the employee's last name?"
-            },
-            {
-                type: 'list',
-                name: 'selectRole',
-                message: "What is the employee's role?",
-                choices: roles
-            },
-            {
-                type: 'list',
-                name: 'selectManager',
-                message: "What is the employee's manager?",
-                choices: managers
+                name: emp.first_name.concat(' ',emp.last_name),
+                value: emp.employee_id
             }
-        ])
-        .then((answer)=>{
-            let query = `INSERT INTO employees (employees.first_name, employees.last_name, roles.title, manager)
-                         VALUE (${answer.newFirstName}, ${answer.newLastName}, ${answer.selectRole}, ${answer.selectManager})
-                         FROM employees
-                         LEFT JOIN employees manager
-                         ON manager.employee_id = employees.manager_id
-                         LEFT JOIN roles
-                         ON employees.role_id = roles.role_id
-                         LEFT JOIN departments
-                         ON roles.department_id = departments.department_id`;
-            // let newEmployee = {
-            //     newFirstName: answer.newFirstName, 
-            //     newLastName: answer.newLastName, 
-            //     selectRole: answer.selectRole, 
-            //     selectManager: answer.selectManager
-            // }
-            connection.query(query, (err, result)=>{
-                if(err){
-                    console.log(err);
+        ))
+
+        rolesArray = res.map(role =>(
+            {
+                name: role.title,
+                value: role.role_id,
+            }
+        ))            
+
+        managersArray = res.map(manager=>(
+            {
+                name: manager.manager,
+                value: manager.manager_id,
+            }
+        )).filter(manager => {return manager !== null})
+
+        console.log(employeesArray)
+        console.log(rolesArray)
+        console.log(managersArray)
+    
+        return inquirer
+            .prompt ([
+                {
+                    type: 'input',
+                    name: 'newFirstName',
+                    message: "What is the employee's first name?",
+                },
+                {
+                    type: 'input',
+                    name: 'newLastName',
+                    message: "What is the employee's last name?"
+                },
+                {
+                    type: 'list',
+                    name: 'selectRole',
+                    message: "What is the employee's role?",
+                    choices: rolesArray
+                },
+                {
+                    type: 'list',
+                    name: 'selectManager',
+                    message: "What is the employee's manager?",
+                    choices: managersArray
                 }
-                console.log(result);
-                viewAllEmployees()
+            ])
+            .then((answer)=>{
+                let sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (? ? ? ?)`;
+                let params = [answer.newFirstName, answer.newLastName, answer.selectRole, answer.selectManager]
+                console.log(params)
+                console.log(answer.selectRole,answer.selectManager)
+                // let newEmployee = {
+                //     newFirstName: answer.newFirstName, 
+                //     newLastName: answer.newLastName, 
+                //     selectRole: answer.selectRole, 
+                //     selectManager: answer.selectManager
+                // }
+                connection.query(sql, params, (err, result)=>{
+                    if(err){
+                        console.log(err);
+                    }
+                    viewAllEmployees()
+                })
             })
-            
-            askUserInput()
-        })
+    })
 }
 
 updateEmployeeRole=()=>{
@@ -239,13 +282,13 @@ updateEmployeeRole=()=>{
                 type: 'list',
                 name: 'selectEmployee',
                 message: "Which employee's role do you want to update?",
-                choices: employees
+                choices: employeesArray
             },
             {
                 type: 'list',
                 name: 'selectRole',
                 message: 'Which role do you want to assign the selected employee?',
-                choices: roles
+                choices: rolesArray
             }
         ])
         .then((answer)=>{
@@ -272,7 +315,6 @@ updateEmployeeRole=()=>{
                 console.log(result);
                 viewAllEmployees()
             })
-            askUserInput()
         })
 }
 
